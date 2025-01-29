04aed7872fa235fc0847f7ad240c7dbf6ddbec41
Jsanzo97 <jorgesanzo21@gmail.com>
2025-01-29 20:20:33 +0100

    Migrate ShowPhoto

---
+++ app/src/main/java/com/jsanzo/figerprintshortcut/ShowPhoto.kt
@@ -0,0 +1,11 @@
+package com.jsanzo.figerprintshortcut
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowPhoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_photo)
+    }
+}
